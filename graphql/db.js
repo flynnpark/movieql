df3a24e506891c0ef1025679d3cea30ccc82fadb
Flynn <wphestiraid@gmail.com>
2018-07-05 20:12:35 +0900

    Defining Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,39 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: 'Flynn',
-    age: 27,
-    gender: 'male'
+    name: 'Star Wars - The new one',
+    score: 1
   },
   {
     id: 1,
-    name: 'Amie',
-    age: 27,
-    gender: 'male'
+    name: 'Avengers - The new one',
+    score: 8
   },
   {
     id: 2,
-    name: 'Dal',
-    age: 33,
-    gender: 'male'
+    name: 'The Godfather I',
+    score: 99
   },
   {
     id: 3,
-    name: 'Yesdoing',
-    age: 27,
-    gender: 'male'
+    name: 'Logan',
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.lendth > cleanedMovies.length) {
+    movie = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
